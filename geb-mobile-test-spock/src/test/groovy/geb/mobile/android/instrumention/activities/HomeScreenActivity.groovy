54221a678b3dcc170bf2d105c85d842f221edc2a
“ShellyXueHan” <shelly@freshworks.io>
2018-02-05 14:47:59 -0800

    replace Java methods with Groovy ones

--- geb-mobile-test-spock/src/test/groovy/geb/mobile/android/instrumention/activities/HomeScreenActivity.groovy
+++ geb-mobile-test-spock/src/test/groovy/geb/mobile/android/instrumention/activities/HomeScreenActivity.groovy
@@ -12,18 +12,7 @@ class HomeScreenActivity extends AndroidBaseActivity {
     static content = {
         searchButton {$("Search Wikipedia")}
 
-        // searchText { 
-        //     AndroidElement insertTextElement = (AndroidElement) new WebDriverWait(driver, 30).until(
-        //         ExpectedConditions.elementToBeClickable(MobileBy.id("org.wikipedia.alpha:id/search_src_text")))
-        //     return insertTextElement
-        // }
-
         searchText {$("#search_src_text")}
-        
-        // resultList {
-        //     List<AndroidElement> allProductsName = driver.findElementsByClassName("android.widget.TextView")
-        //     return allProductsName
-        // }
 
         resultList {$("android.widget.TextView")}
     }
