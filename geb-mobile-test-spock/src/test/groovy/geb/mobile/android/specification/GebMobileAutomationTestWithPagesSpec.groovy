8b7a4e7b1d58ee283df9b8c6f061e630af1ed8c4
“ShellyXueHan” <shelly@freshworks.io>
2018-02-14 17:25:32 -0800

    updated test case for android

---
+++ geb-mobile-test-spock/src/test/groovy/geb/mobile/android/specification/GebMobileAutomationTestWithPagesSpec.groovy
@@ -0,0 +1,37 @@
+package geb.mobile.android.specification
+
+import geb.mobile.GebMobileBaseSpec
+import geb.mobile.android.activities.HomeScreenActivity
+//import geb.mobile.android.activities.RegisterUserActivity
+//import geb.mobile.android.activities.VerifyUserActivity
+//import geb.mobile.android.activities.WebViewActivity
+import spock.lang.Stepwise
+
+/**
+ * Created by gmueksch on 23.06.14.
+ */
+
+
+
+/**
+ * Sample test case for android app on Wiki native app:
+ * TODO: rename activity to match the app's current activity
+ */
+@Stepwise
+class GebMobileAutomationTestWithPagesSpec extends GebMobileBaseSpec {
+
+    def "open test-app and enter search string "() {
+        given: "I land on Home screen"
+        at HomeScreenActivity
+
+        when: "I click on the search button"
+        searchButton.click()
+
+        and: "I type in search string"
+        searchText = "BrowserStack"
+
+        then: "I should see results"
+        assert resultList.size() > 0
+    }
+
+}
