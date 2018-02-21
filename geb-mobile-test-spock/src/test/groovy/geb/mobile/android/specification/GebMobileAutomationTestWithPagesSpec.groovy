93aa1bcae48a9e79dc4ca0533e8a016442899456
“ShellyXueHan” <shelly@freshworks.io>
2018-02-21 12:14:32 -0800

    added huawei device camera activity

--- geb-mobile-test-spock/src/test/groovy/geb/mobile/android/specification/GebMobileAutomationTestWithPagesSpec.groovy
+++ geb-mobile-test-spock/src/test/groovy/geb/mobile/android/specification/GebMobileAutomationTestWithPagesSpec.groovy
@@ -2,6 +2,8 @@ package geb.mobile.android.specification
 
 import geb.mobile.GebMobileBaseSpec
 import geb.mobile.android.activities.HomeScreenActivity
+import geb.mobile.android.activities.CameraActivity
+
 import spock.lang.Stepwise
 
 
@@ -33,4 +35,21 @@ class GebMobileAutomationTestWithPagesSpec extends GebMobileBaseSpec {
         assert resultList.size() > 0
     }
 
+
+
+//    def "test camera and take photo"() {
+//        given: "open the camera"
+//        sleep(3000)
+//        at CameraActivity
+//
+//        when: "I enable the location request"
+//        acceptButton.click()
+//
+//        and: "I click on the shutter button"
+//        sleep(1000)
+//        shutterButton.click()
+//
+//        then: "I should have the camera ready again"
+//        preview
+//    }
 }
